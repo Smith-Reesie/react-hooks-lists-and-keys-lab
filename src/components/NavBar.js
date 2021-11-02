d4d5480c50021f32175513fca561531726aaeae4
Maurice Smith <Xmaurice.smith@gmail.com>
2021-11-02 13:54:47 -0400

    commit tree 84f9f00cc7d1324aa0151fdd320af7cab531a122

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,22 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {links.map(link => <a key={link} href={`#${link}`}>{link}</a>)}
+  </nav>;
+
+  
+
+
+
+  //create element for each link 
 }
 
 export default NavBar;
+
+
+
+
+// . map creates a copy of the orginal array 
+// .forEach doesnt return anything 
+// .map returns a new array altered for our purposes 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+
+  const mappedProjects = projects.map(project => 
+  <ProjectItem 
+  key={project.id}
+  about={project.about}
+  name={project.name} 
+  technologies={project.technologies} />
+  )
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {mappedProjects}
+      </div>
     </div>
   );
 }
